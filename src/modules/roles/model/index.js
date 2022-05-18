de7857547fe873accc7f3209c4e262b838ee5bda
manarArabi <manar.araby.ma@gmail.com>
2022-05-18 21:12:20 +0200

    add user and role models

---
+++ src/modules/roles/model/index.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const roleSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+
+  permissions: {
+    type: [String], 
+    required: true 
+  }
+})
+
+export const Roles = mongoose.model('roles', roleSchema)
---
+++ src/modules/users/model/index.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+
+  password: {
+    type: String, // hashed
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+
+  role: {
+    _id: {
+      type: mongoose.Types.ObjectId,
+      ref: 'roles'
+    },
+
+    permissions: [{
+      type: String
+    }]
+  }
+})
+
+export const Users = mongoose.model('roles', roleSchema)
