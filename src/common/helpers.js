47e8c104ec7e2ab0b66f10c47255e3d2081b5d2a
manarArabi <manar.araby.ma@gmail.com>
2022-05-19 04:23:33 +0200

    add generate jwt helper

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -1,9 +1,12 @@
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 /**
  * It hashes a string
  *
- * @param {String} string
+ * @param {Object} args
+ * @param {String} args.str
+ * @param {Number} [args.rounds = 3]
  *
  * @returns {Promise<String>}
  */
@@ -11,4 +14,19 @@ export const hashString = async ({ str, rounds = 3 }) => {
   const hashedString = await bcrypt.hash(str, rounds)
 
   return hashedString
+}
+
+/**
+ * It generates a jwt with the provided data
+ *
+ * @param {Object} args
+ * @param {Object} args.data
+ * @param {Object} args.jwtOptions
+ *
+ * @returns {String}
+ */
+export const generateJwt = ({ data, jwtOptions = {} }) => {
+  const token = jwt.sign(data, process.env.JWT_SECRET, { ...jwtOptions, expiresIn: '2 days' })
+
+  return token
 }
