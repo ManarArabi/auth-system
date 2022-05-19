e599be2db657fc45ed1bc86deb023656272e151c
manarArabi <manar.araby.ma@gmail.com>
2022-05-19 23:41:16 +0200

    add more base permissions

--- src/modules/permissions/constants/index.js
+++ src/modules/permissions/constants/index.js
@@ -1,9 +1,15 @@
 const CAN_ASSIGN_USER_ROLE = 'can-assign-user-role'
 const CAN_ADD_ROLE = 'can-add-role'
 const CAN_ADD_PERMISSION = 'can-add-permission'
+const CAN_ASSIGN_USER_PERMISSION = 'can-assign-user-permission'
+const CAN_ASSIGN_ROLE_PERMISSION = 'can-assign-role-permission'
+const CAN_ADD_ACTION = 'can-add-action'
 
 export const BASIC_ADMIN_PERMISSIONS = [
   CAN_ASSIGN_USER_ROLE,
   CAN_ADD_ROLE,
-  CAN_ADD_PERMISSION
+  CAN_ADD_PERMISSION,
+  CAN_ASSIGN_USER_PERMISSION,
+  CAN_ASSIGN_ROLE_PERMISSION,
+  CAN_ADD_ACTION
 ]
