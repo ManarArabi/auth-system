e4b16e412d96399c5f44a86c43893252dfcaa48c
manarArabi <manar.araby.ma@gmail.com>
2022-05-20 20:41:33 +0200

    attach listeners

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import app from './app.js'
 import mongoose from 'mongoose'
 import { config } from 'dotenv'
 import { seedSystemSetup } from './seeding.js'
+import { subscribeUsersEventListeners } from './modules/users/scripts/event-subscribing.js'
 
 const port = process.env.PORT || 3000
 
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URL).then(async () => {
   console.log('Mongoose is running')
 
   await seedSystemSetup().catch((err) => console.log(err))
+  subscribeUsersEventListeners()
 }).catch(() => {
   console.log('Failed to start mongoose')
 })
